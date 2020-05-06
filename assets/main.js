cf77d32ce88ab2558c505f1551a2b6d942563357
Jon Taylor <jontaylor224@gmail.com>
2020-05-06 15:03:48 -0500

    guard clause for non-alpha chars

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,13 @@
-const wordToGuessElement = document.querySelector('#word-to-guess')
-const letterInputElement = document.querySelector('#letter-input')
-const letterSubmitButton = document.querySelector('#letter-submit-button')
-const lettersAttemptedElement = document.querySelector('#letters-attempted')
+const wordToGuessElement = document.querySelector("#word-to-guess")
+const letterInputElement = document.querySelector("#letter-input")
+const letterSubmitButton = document.querySelector("#letter-submit-button")
+const lettersAttemptedElement = document.querySelector("#letters-attempted")
 const guessesRemainingElement = document.querySelector("#guesses-remaining")
-
+const letters = /^[A-Za-z]+$/
 const guessesAllowed = 6
-guessesRemainingElement.innerText = ' ' + guessesAllowed
+guessesRemainingElement.innerText = " " + guessesAllowed
 
-let currentWord = '_'.repeat(targetWord.length)
+let currentWord = "+".repeat(targetWord.length)
 wordToGuessElement.innerText = currentWord
 
 let wrongLetterCount = 0
@@ -15,33 +15,39 @@ let gameOver = false
 
 // console.log(targetWord)
 
-letterSubmitButton.addEventListener('click', function(){
+letterSubmitButton.addEventListener("click", function () {
     if (!gameOver) {
-        let lettersGuessed = lettersAttemptedElement.innerText
-        let inputLetter = letterInputElement.value.toUpperCase()
-        if (!lettersGuessed.includes(inputLetter)){
-            lettersAttemptedElement.innerText += inputLetter
-            if (targetWord.includes(inputLetter)) {
-                for (let i = 0; i < targetWord.length; i++) {
-                    if (targetWord[i] === inputLetter){
-                        currentWord = currentWord.substr(0,i) + inputLetter + currentWord.substr(i+1)
+        if (letterInputElement.value.match(letters)) {
+            let lettersGuessed = lettersAttemptedElement.innerText
+            let inputLetter = letterInputElement.value.toUpperCase()
+            if (!lettersGuessed.includes(inputLetter)) {
+                lettersAttemptedElement.innerText += inputLetter
+                if (targetWord.includes(inputLetter)) {
+                    for (let i = 0; i < targetWord.length; i++) {
+                        if (targetWord[i] === inputLetter) {
+                            currentWord =
+                                currentWord.substr(0, i) +
+                                inputLetter +
+                                currentWord.substr(i + 1)
+                        }
                     }
+                    wordToGuessElement.innerText = currentWord
+                } else {
+                    wrongLetterCount++
+                    let newGuessCount = guessesAllowed - wrongLetterCount
+                    guessesRemainingElement.innerText = " " + newGuessCount
                 }
-                wordToGuessElement.innerText = currentWord
             }
-            else {
-                wrongLetterCount++
-                let newGuessCount = guessesAllowed - wrongLetterCount
-                guessesRemainingElement.innerText = ' ' + newGuessCount
+            if (currentWord === targetWord) {
+                gameOver = true
+                alert("You Win!  Reload page to play again.")
             }
-        }
-        if ( currentWord === targetWord){
-            gameOver = true
-            alert("You Win!  Reload page to play again.")
-        }
-        if (wrongLetterCount === guessesAllowed) {
-            gameOver = true
-            alert("You lose.  Reload page to play again.")
+            if (wrongLetterCount === guessesAllowed) {
+                gameOver = true
+                alert("You lose.  Reload page to play again.")
+            }
+        }else {
+            alert("Please input a letter.")
         }
         letterInputElement.value = null
     }
