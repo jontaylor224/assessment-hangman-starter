d90e5c2df71201162fbb4873d952f03f1791e902
Jon Taylor <jontaylor224@gmail.com>
2020-05-06 14:39:21 -0500

    implemented game functionality

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,48 @@
 const wordToGuessElement = document.querySelector('#word-to-guess')
-const currentWord = wordToGuessElement.innerText
+const letterInputElement = document.querySelector('#letter-input')
+const letterSubmitButton = document.querySelector('#letter-submit-button')
+const lettersAttemptedElement = document.querySelector('#letters-attempted')
+const guessesRemainingElement = document.querySelector("#guesses-remaining")
 
-// const letterInputElement = document.querySelector('#letter-input')
-// const letterSubmitButton = document.querySelector('#letter-submit-button')
-// const lettersAttemptedElement = document.querySelector('#letters-attempted')
+const guessesAllowed = 6
+guessesRemainingElement.innerText = ' ' + guessesAllowed
+
+let currentWord = '_'.repeat(targetWord.length)
+wordToGuessElement.innerText = currentWord
+
+let wrongLetterCount = 0
+let gameOver = false
+
+console.log(targetWord)
+
+letterSubmitButton.addEventListener('click', function(){
+    if (!gameOver) {
+        let lettersGuessed = lettersAttemptedElement.innerText
+        let inputLetter = letterInputElement.value.toUpperCase()
+        if (!lettersGuessed.includes(inputLetter)){
+            lettersAttemptedElement.innerText += inputLetter
+            if (targetWord.includes(inputLetter)) {
+                for (let i = 0; i < targetWord.length; i++) {
+                    if (targetWord[i] === inputLetter){
+                        currentWord = currentWord.substr(0,i) + inputLetter + currentWord.substr(i+1)
+                    }
+                }
+                wordToGuessElement.innerText = currentWord
+            }
+            else {
+                wrongLetterCount++
+                let newGuessCount = guessesAllowed - wrongLetterCount
+                guessesRemainingElement.innerText = ' ' + newGuessCount
+            }
+        }
+        if ( currentWord === targetWord){
+            gameOver = true
+            alert("You Win!  Reload page to play again.")
+        }
+        if (wrongLetterCount === guessesAllowed) {
+            gameOver = true
+            alert("You lose.  Reload page to play again.")
+        }
+        letterInputElement.value = null
+    }
+})
