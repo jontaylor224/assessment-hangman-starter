33610acd87f0e102aa7fb1052e89b45bba963385
Jon Taylor <jontaylor224@gmail.com>
2020-05-06 15:17:52 -0500

    changed gameOver flag check to guard clause

--- assets/main.js
+++ assets/main.js
@@ -16,39 +16,40 @@ let gameOver = false
 // console.log(targetWord)
 
 letterSubmitButton.addEventListener("click", function () {
-    if (!gameOver) {
-        if (letterInputElement.value.match(letters)) {
-            let lettersGuessed = lettersAttemptedElement.innerText
-            let inputLetter = letterInputElement.value.toUpperCase()
-            if (!lettersGuessed.includes(inputLetter)) {
-                lettersAttemptedElement.innerText += inputLetter
-                if (targetWord.includes(inputLetter)) {
-                    for (let i = 0; i < targetWord.length; i++) {
-                        if (targetWord[i] === inputLetter) {
-                            currentWord =
-                                currentWord.substr(0, i) +
-                                inputLetter +
-                                currentWord.substr(i + 1)
-                        }
-                    }
-                    wordToGuessElement.innerText = currentWord
-                } else {
-                    wrongLetterCount++
-                    let newGuessCount = guessesAllowed - wrongLetterCount
-                    guessesRemainingElement.innerText = " " + newGuessCount
-                }
-            }
-            if (currentWord === targetWord) {
-                gameOver = true
-                alert("You Win!  Reload page to play again.")
-            }
-            if (wrongLetterCount === guessesAllowed) {
-                gameOver = true
-                alert("You lose.  Reload page to play again.")
-            }
-        }else {
-            alert("Please input a letter.")
-        }
-        letterInputElement.value = null
+    if (gameOver) {
+        return
     }
+    if (letterInputElement.value.match(letters)) {
+        let lettersGuessed = lettersAttemptedElement.innerText
+        let inputLetter = letterInputElement.value.toUpperCase()
+        if (!lettersGuessed.includes(inputLetter)) {
+            lettersAttemptedElement.innerText += inputLetter
+            if (targetWord.includes(inputLetter)) {
+                for (let i = 0; i < targetWord.length; i++) {
+                    if (targetWord[i] === inputLetter) {
+                        currentWord =
+                            currentWord.substr(0, i) +
+                            inputLetter +
+                            currentWord.substr(i + 1)
+                    }
+                }
+                wordToGuessElement.innerText = currentWord
+            } else {
+                wrongLetterCount++
+                let newGuessCount = guessesAllowed - wrongLetterCount
+                guessesRemainingElement.innerText = " " + newGuessCount
+            }
+        }
+        if (currentWord === targetWord) {
+            gameOver = true
+            alert("You Win!  Reload page to play again.")
+        }
+        if (wrongLetterCount === guessesAllowed) {
+            gameOver = true
+            alert("You lose.  Reload page to play again.")
+        }
+    } else {
+        alert("Please input a letter.")
+    }
+    letterInputElement.value = null
 })
